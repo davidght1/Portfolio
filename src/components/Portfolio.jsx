d7fa5bc7d6c4150979d3f182e99aa84fd990bb96
David Agam <davidght1@gmail.com>
2023-02-11 15:41:00 +0200

    modified:   package-lock.json 	modified:   package.json 	new file:   public/resume.pdf 	deleted:    src/App.css 	modified:   src/App.js 	deleted:    src/App.test.js 	new file:   src/assets/css.png 	new file:   src/assets/github.png 	new file:   src/assets/heroImage.png 	new file:   src/assets/html.png 	new file:   src/assets/java.png 	new file:   src/assets/javascript.png 	new file:   src/assets/mongodb.png 	new file:   src/assets/mysql.png 	new file:   src/assets/node.png 	new file:   src/assets/portfolio/amazona.jpg 	new file:   src/assets/portfolio/arrayDestruct.jpg 	new file:   src/assets/portfolio/blog.jpg 	new file:   src/assets/portfolio/installNode.jpg 	new file:   src/assets/portfolio/navbar.jpg 	new file:   src/assets/portfolio/reactParallax.jpg 	new file:   src/assets/portfolio/reactSmooth.jpg 	new file:   src/assets/portfolio/reactWeather.jpg 	new file:   src/assets/portfolio/usestate.jpg 	new file:   src/assets/react.png 	new file:   src/assets/tailwind.png 	new file:   src/components/Contact.jsx 	new file:   src/components/Experience.jsx 	new file:   src/components/Home.jsx 	new file:   src/components/NavBar.jsx 	new file:   src/components/Portfolio.jsx 	new file:   src/components/SocialLinks.jsx 	modified:   src/index.css 	modified:   src/index.js 	deleted:    src/logo.svg 	deleted:    src/reportWebVitals.js 	deleted:    src/setupTests.js 	new file:   tailwind.config.js

---
+++ src/components/Portfolio.jsx
@@ -0,0 +1,75 @@
+import React from "react";
+import amazona from "../assets/portfolio/amazona.jpg";
+import blog from "../assets/portfolio/blog.jpg";
+import navbar from "../assets/portfolio/navbar.jpg";
+const Portfolio = () => {
+  const portfolios = [
+    {
+      id: 0,
+      src: amazona,
+      demo: "https://mern-amazona-app-2gxt.onrender.com",
+      code: "https://github.com/davidght1/amazonMern",
+    },
+    {
+      id: 1,
+      src: blog,
+      code: "https://github.com/davidght1/Mern-Blog",
+    },
+    {
+      id: 2,
+      src: navbar,
+    },
+  ];
+  const handleDemoClick = (url) => {
+    window.open(url, "_blank");
+  };
+
+  const handleCodeClick = (url) => {
+    window.open(url, "_blank");
+  };
+  return (
+    <div
+      name="portfolio"
+      className="bg-gradient-to-b from-gray-800 to-black w-full text-white md:h-screen"
+    >
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+        <div className="pd-8">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+            Portfolio
+          </p>
+          <p className="py-6">Check out some of my work right here</p>
+        </div>
+
+        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
+          {portfolios.map(({ id, src, demo, code }) => (
+            <div key={id} className="shadow-md shadow-gray-600 rounded-lg">
+              <img
+                src={src}
+                alt="MyProjects"
+                className="rounded-md duration-200 hover:scale-105"
+              ></img>
+              <div className="flex items-center justify-center">
+                <button
+                  onClick={() => handleDemoClick(demo)}
+                  disabled={!demo}
+                  className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105"
+                >
+                  Demo
+                </button>
+                <button
+                  onClick={() => handleCodeClick(code)}
+                  disabled={!code}
+                  className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105"
+                >
+                  Code
+                </button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Portfolio;
