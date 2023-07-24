689d65bcdd4aa2148f1084da9f0a211776bfee6f
David Agam <davidght1@gmail.com>
2023-07-24 20:12:15 +0300

    new file:   src/assets/portfolio/fashion.jpg 	modified:   src/components/Portfolio.jsx

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -4,6 +4,7 @@ import blog from "../assets/portfolio/blog.jpg";
 import pinvent from "../assets/portfolio/pinvent.jpg";
 import sport from "../assets/portfolio/sport.jpg";
 import rick from "../assets/portfolio/rick.jpg";
+import fashion from '../assets/portfolio/fashion.jpg'
 const Portfolio = () => {
   const portfolios = [
     {
@@ -14,24 +15,30 @@ const Portfolio = () => {
     },
     {
       id: 1,
+      src: fashion,
+      demo: "https://ecommerce-shop-starter-main.vercel.app/",
+      code: "https://github.com/davidght1/ecommerce-shop-starter-main",
+    },
+    {
+      id: 2,
       src: amazona,
       demo: "https://mern-amazona-app-2gxt.onrender.com",
       code: "https://github.com/davidght1/amazonMern",
     },
     {
-      id: 2,
+      id: 3,
       src: rick,
       demo: "https://rick-and-morty-davidght1.vercel.app",
       code: "https://github.com/davidght1/RickAndMorty",
     },
     {
-      id: 3,
+      id: 4,
       src: sport,
       demo: "https://sports-task.vercel.app",
       code: "https://github.com/davidght1/SportsTask",
     },
     {
-      id: 4,
+      id: 5,
       src: blog,
       code: "https://github.com/davidght1/Mern-Blog",
     },
